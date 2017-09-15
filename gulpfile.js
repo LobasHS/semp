9ade48f3c6ba8bfdeec69ddd3c92c4d483412265
LobasHS <lobas.hennadiy.com>
2017-09-15 23:50:06 +0300

    Add gulp-sourcemaps, gulp-autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ var sass = require('gulp-sass');
 var spritesmith = require('gulp.spritesmith');
 var rimraf = require('rimraf');
 var rename = require('gulp-rename');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 //server
 gulp.task('server', function() {
@@ -19,7 +21,7 @@ gulp.task('server', function() {
 
 //pug compile
 gulp.task('templates:compile', function buildProjectHTML() {
-    return gulp.src('sourse/template/index.pug')
+    return gulp.src('src/template/index.pug')
         .pipe(pug({
             pretty: true
         }))
@@ -28,21 +30,24 @@ gulp.task('templates:compile', function buildProjectHTML() {
 
 //styles compile
 gulp.task('styles:compile', function () {
-    return gulp.src('sourse/styles/main.scss')
+    return gulp.src('src/styles/main.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle:'compressed'}).on('error', sass.logError))
+        .pipe(autoprefixer())
         .pipe(rename('main.min.css'))
+        .pipe(sourcemaps.write('/maps'))
         .pipe(gulp.dest('buildProject/css'));
 });
 
 //sprites
 gulp.task('sprite', function (cb) {
-    var spriteData = gulp.src('sourse/images/icom/*.png').pipe(spritesmith({
+    var spriteData = gulp.src('src/images/icom/*.png').pipe(spritesmith({
         imgName: 'sprite.png',
-        imgPath: 'sourse/images/sprite.png',
+        imgPath: 'src/images/sprite.png',
         cssName: 'sprite.scss'
     }));
     spriteData.img.pipe(gulp.dest('buildProject/images/'));
-    spriteData.css.pipe(gulp.dest('sourse/styles/global/'));
+    spriteData.css.pipe(gulp.dest('src/styles/global/'));
     cb();
 });
 
@@ -53,13 +58,13 @@ gulp.task("clean",function del(cb) {
 
 //copy fonts
 gulp.task('copy:fonts',function () {
-    return gulp.src('./sourse/fonts/**/*.*')
+    return gulp.src('./src/fonts/**/*.*')
         .pipe(gulp.dest('buildProject/fonts'))
 });
 
 //copy images
 gulp.task('copy:images',function () {
-    return gulp.src('./sourse/images/**/*.*')
+    return gulp.src('./src/images/**/*.*')
         .pipe(gulp.dest('buildProject/images'))
 });
 
@@ -68,8 +73,8 @@ gulp.task('copy',gulp.parallel('copy:fonts','copy:images'));
 
 //watchers
 gulp.task('watch',function () {
-    gulp.watch('sourse/template/**/*.pug', gulp.series('templates:compile'));
-    gulp.watch('sourse/styles/**/*.scss', gulp.series('styles:compile'));
+    gulp.watch('src/template/**/*.pug', gulp.series('templates:compile'));
+    gulp.watch('src/styles/**/*.scss', gulp.series('styles:compile'));
 });
 
 //default
